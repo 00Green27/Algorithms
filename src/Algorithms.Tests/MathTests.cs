04b5d1fd7505b22db8eda6ad40922f8a61fe61d0
00Green27 <patrick17@mail.ru>
2019-01-22 23:41:04 +0300

    Greatest common divisor

---
+++ src/Algorithms.Tests/MathTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace Algorithms {
+  public class MathTests {
+
+    [Theory]
+    [InlineData(48, 60, 12)]
+    [InlineData(63, 81, 9)]
+    [InlineData(242, 132, 22)]
+    [InlineData(2625, 8100, 75)]
+    [InlineData(999, 0, 999)]
+    public void Gsd(int a, int b, int gsd) {
+      var result = Math.Gsd(a, b);
+      Assert.Equal(gsd, result);
+    }
+  }
+}
---
+++ src/Algorithms/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Algorithms
+{
+  public static class Math
+  {
+    public static int Gsd(int a, int b)
+    {
+      int x = a, y = b, remainder = 0;
+      while (y != 0)
+      {
+        remainder = x % y;
+        x = y;
+        y = remainder;
+      }
+
+      return x;
+    }
+  }
+}
