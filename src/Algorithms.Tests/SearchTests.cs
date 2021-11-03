9e1eba6473b584f95bee59191dd1a9966d5d2174
Ed Dagaev <ed.dagaev@gmail.com>
2021-11-03 10:52:06 +0300

    Add BinarySearch

---
+++ src/Algorithms.Tests/SearchTests.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using Xunit;
+
+namespace Algorithms
+{
+	public class SearchTests
+	{
+		[Theory]
+		[InlineData(1, 1, 5, 0)]
+		[InlineData(5, 1, 5, 4)]
+		[InlineData(4, 1, 10, 3)]
+		[InlineData(41, 1, 100, 40)]
+		[InlineData(2, 1, 2, 1)]
+		[InlineData(-999, 1, 100, null)]
+		[InlineData(999, 1, 100, null)]
+		public void BinarySearch(int item, int min, int max, int? result)
+		{
+			var found = Search.BinarySearch(item, Enumerable.Range(min, max).ToArray());
+			Assert.Equal(found, result);
+		}
+	}
+}
---
+++ src/Algorithms/Search.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public static class Search
+	{
+		public static int? BinarySearch(int item, IList<int> values)
+		{
+			int low = 0;
+			int hight = values.Count - 1;
+
+			while (low <= hight)
+			{
+				int mid = (hight + low) / 2;
+				if (values[mid] == item)
+				{
+					return mid;
+				}
+
+				if (values[mid] < item)
+				{
+					low = mid + 1;
+				}
+				else
+				{
+					hight = mid - 1;
+				}
+			}
+
+			return null;
+		}
+	}
+}
